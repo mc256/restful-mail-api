73f2fb2f902dccd43f2ae533a6b82d80c990d030
masterchan <masterchan.256@gmail.com>
2019-02-18 19:30:47 -0500

    ADD: domain model (database)

--- model/domain.js
+++ model/domain.js
@@ -2,15 +2,19 @@ const db_pool = require('./database');
 
 class domain_operation{
 
-    static async list_domain(keyword){
+    static async list_domain(keyword = '', offset = 0) {
         let connection;
         try {
-             connection = await db_pool.getConnection();
-            if (keyword === undefined){
-                return await connection.query('SELECT * FROM `domains`');
-            }else{
-                return await connection.query('SELECT * FROM `domains` WHERE `domain` LIKE ?', ['%'+ keyword +'%']);
-            }
+            connection = await db_pool.getConnection();
+            let result;
+            result = await connection.query('SELECT * FROM `domains` WHERE `domain` LIKE ? LIMIT ? OFFSET ?',
+                [
+                    '%' + keyword + '%',
+                    parseInt(process.env.DEFAULT_PAGE_SIZE),
+                    offset
+                ]
+            );
+            return JSON.stringify(result);
         }catch (e) {
             throw e;
         }finally {
@@ -22,11 +26,24 @@ class domain_operation{
         let connection;
         try {
             connection = await db_pool.getConnection();
-            if (domain_name === undefined){
-
-            }else{
-                return await connection.query('INSERT INTO `domains` (`domain`) VALUES (?)');
+            let result = await connection.query('INSERT INTO `domains` (`domain`) VALUES (?);', [domain_name]);
+            return JSON.stringify({"affectedRows": result.affectedRows});
+        }catch (e) {
+            if (e.code.toString() === 'ER_DUP_ENTRY'){
+                return JSON.stringify({"affectedRows":0});
             }
+            throw e;
+        }finally {
+            if (connection) connection.end();
+        }
+    }
+
+    static async delete_domain(domain_name){
+        let connection;
+        try {
+            connection = await db_pool.getConnection();
+            let result = await connection.query('DELETE FROM `domains` WHERE `domains`.`domain` = ?', [domain_name]);
+            return JSON.stringify({"affectedRows": result.affectedRows});
         }catch (e) {
             throw e;
         }finally {
